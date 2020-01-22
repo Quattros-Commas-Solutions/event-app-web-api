8c9317a7d98d7215a8c936792aead9418faf51fb
zrilman <dragec.96@live.com>
2020-01-22 13:45:43 +0100

    Refactor auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,44 +4,63 @@ const ValidationUtil = require('../util/validationUtil');
 
 const HttpStatus = require('http-status-codes');
 
+const authUser = (req, res, next) => {
+    return authHelper(req, res, next);
+};
 
-const auth = (checkAccessType) => {
-    return (req, res, next) => {
-        const token = req.headers['x-access-token'] || req.headers['authorization'];
+const authAdmin = (req, res, next) => {
+    return authHelper(req, res, next, ValidationUtil.isUserAdmin);
+};
 
-        // Decode token
-        if (token) {
-            // Remove unwanted prefix if exists
-            if (token.startsWith('Bearer ')) {
-                token = token.slice(7, token.length);
-            }
-            // Verify token
-            jwt.verify(token, AppConfig.SECRET, function (err, decoded) {
-                if (!err) {
-                    // If checkAccessType is true => (super)admin-specific route
-                    if (checkAccessType && ValidationUtil.isUserAdmin(decoded.accessType)) {
-                        req.decoded = decoded;
-                        next();
-                    }
-                    // If checkAccessType is false => available for all logged users
-                    else if (!checkAccessType) {
-                        req.decoded = decoded;
-                        next();
-                    } else {
-                        // Logged user can't access (super)admin-specific routes
-                        return res.status(HttpStatus.UNAUTHORIZED).json({ success: false, message: 'Unauthorized access.' });
-                    }
-                } else {
-                    res.status(HttpStatus.UNAUTHORIZED).json({ error: true, success: false, message: 'Unauthorized access.' });
-                }
-            });
-        } else {
-            res.status(HttpStatus.FORBIDDEN).json({ error: true, success: false, message: 'No token provided.' });
+const authHelper = (req, res, next, checkAuthorizationCallback) => {
+
+    const token = req.headers['x-access-token'] || req.headers['authorization'];
+
+    // Decode token
+    if (token) {
+        // Remove unwanted prefix if exists
+        if (token.startsWith('Bearer ')) {
+            token = token.slice(7, token.length);
         }
+        // Verify token
+        jwt.verify(token, AppConfig.SECRET, function (err, decoded) {
+            if (!err) {
+                // If checkAccessType is true => (super)admin-specific route
+                if (checkAuthorizationCallback && checkAuthorizationCallback(decoded.accessType)) {
+                    req.decoded = decoded;
+                    next();
+                }
+                // If checkAccessType is false => available for all logged users
+                else if (!checkAuthorizationCallback) {
+                    req.decoded = decoded;
+                    next();
+                } else {
+                    // Logged user can't access (super)admin-specific routes
+                    return res.status(HttpStatus.UNAUTHORIZED).json({
+                        success: false,
+                        message: 'Unauthorized access.'
+                    });
+                }
+            } else {
+                res.status(HttpStatus.UNAUTHORIZED).json({
+                    error: true,
+                    success: false,
+                    message: 'Unauthorized access.'
+                });
+            }
+        });
+    } else {
+        res.status(HttpStatus.FORBIDDEN).json({
+            error: true,
+            success: false,
+            message: 'No token provided.'
+        });
     }
+
 }
 
 
 module.exports = {
-    auth
-} 
+    authUser,
+    authAdmin
+};
--- routes.js
+++ routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const HttpStatus = require('http-status-codes');
-const jwt = require('jsonwebtoken');
-const auth = require('./middleware/auth').auth;
+const auth = require('./middleware/auth');
 
 //Importing controllers
 const userTypeController = require('./controller/userTypeController');
@@ -11,7 +10,7 @@ const eventQuestionController = require('./controller/eventQuestionController');
 const eventGropController = require('./controller/eventGroupController');
 
 //Setting the default route
-router.get('/', auth, (req, res) => {
+router.get('/', auth.authUser, (req, res) => {
     res.status(HttpStatus.OK).json({
         status: 'Success',
         message: 'Event App root route is working!'
@@ -21,40 +20,40 @@ router.get('/', auth, (req, res) => {
 
 // User routes 
 // TODO: Add auth middleware when JWT is finished
-router.get('/user/', auth(true), (req, res) => userController.retrieveAll(req, res));
-router.post('/user/', auth(true), (req, res) => userController.create(req, res));
-router.get('/user/:id', auth(true), (req, res) => userController.retrieveById(req, res));
-router.patch('/user/update', auth, (req, res) => userController.update(req, res));
-router.delete('/user/:id', auth(true), (req, res) => userController.remove(req, res));
+router.get('/user/', auth.authAdmin, (req, res) => userController.retrieveAll(req, res));
+router.post('/user/', auth.authAdmin, (req, res) => userController.create(req, res));
+router.get('/user/:id', auth.authAdmin, (req, res) => userController.retrieveById(req, res));
+router.patch('/user/update', auth.authUser, (req, res) => userController.update(req, res));
+router.delete('/user/:id', auth.authAdmin, (req, res) => userController.remove(req, res));
 router.post('/user/login', (req, res) => userController.login(req, res));
-router.get('/user/token', auth, (req, res) => userController.token(req, res));
+router.get('/user/token', auth.authUser, (req, res) => userController.token(req, res));
 
 //UserType routes
-router.get('/user-types', auth(true), userTypeController.getAll);
+router.get('/user-types', auth.authUser, userTypeController.getAll);
 
 // Company routes
-router.post('/company', auth(true), (req, res) => companyController.create(req, res));
-router.get('/company', auth(true), (req, res) => companyController.getAll(req, res));
-router.get('/company/:id', auth(true), (req, res) => companyController.getById(req, res));
-router.patch('/company', auth(true), (req, res) => companyController.update(req, res));
-router.delete('/company/:id', auth(true), (req, res) => companyController.deleteCompany(req, res));
-router.get('/company/get-by-name/:name', auth(true), (req, res) => companyController.getByNameContains(req, res));
+router.post('/company', auth.authAdmin, (req, res) => companyController.create(req, res));
+router.get('/company', auth.authAdmin, (req, res) => companyController.getAll(req, res));
+router.get('/company/:id', auth.authAdmin, (req, res) => companyController.getById(req, res));
+router.patch('/company', auth.authAdmin, (req, res) => companyController.update(req, res));
+router.delete('/company/:id', auth.authAdmin, (req, res) => companyController.deleteCompany(req, res));
+router.get('/company/get-by-name/:name', auth.authAdmin, (req, res) => companyController.getByNameContains(req, res));
 
 // EventQuestion routes 
-router.post('/event-question', auth, (req, res) => eventQuestionController.create(req, res));
-router.get('/event-question', auth, (req, res) => eventQuestionController.getAll(req, res));
-router.get('/event-question/:id', auth(true), (req, res) => eventQuestionController.getById(req, res));
-router.delete('/event-question/:id', auth(true), (req, res) => eventQuestionController.deleteById(req, res));
-router.patch('/event-question', auth, (req, res) => eventQuestionController.update(req, res));
-router.post('/event-question/response', auth, (req, res) => eventQuestionController.addResponseToEvent(req, res));
-router.delete('/event-question/response/:eventQuestionId/:responseId', auth(true), (req, res) => eventQuestionController.deleteResponse(req, res));
+router.post('/event-question', auth.authUser, (req, res) => eventQuestionController.create(req, res));
+router.get('/event-question', auth.authUser, (req, res) => eventQuestionController.getAll(req, res));
+router.get('/event-question/:id', auth.authAdmin, (req, res) => eventQuestionController.getById(req, res));
+router.delete('/event-question/:id', auth.authAdmin, (req, res) => eventQuestionController.deleteById(req, res));
+router.patch('/event-question', auth.authUser, (req, res) => eventQuestionController.update(req, res));
+router.post('/event-question/response', auth.authUser, (req, res) => eventQuestionController.addResponseToEvent(req, res));
+router.delete('/event-question/response/:eventQuestionId/:responseId', auth.authAdmin, (req, res) => eventQuestionController.deleteResponse(req, res));
 
 // EventGroup routes
-router.get('/event-group', auth, (req, res) => eventGropController.retrieveAll(req, res))
-router.post('/event-group', auth(true), (req, res) => eventGropController.create(req, res))
-router.get('/event-group/:id', auth, (req, res) => eventGropController.retrieveById(req, res))
-router.patch('/event-group', auth(true), (req, res) => eventGropController.update(req, res))
-router.delete('/event-group/:id', auth(true), (req, res) => eventGropController.remove(req, res))
+router.get('/event-group', auth.authUser, (req, res) => eventGropController.retrieveAll(req, res))
+router.post('/event-group', auth.authAdmin, (req, res) => eventGropController.create(req, res))
+router.get('/event-group/:id', auth.authUser, (req, res) => eventGropController.retrieveById(req, res))
+router.patch('/event-group', auth.authAdmin, (req, res) => eventGropController.update(req, res))
+router.delete('/event-group/:id', auth.authAdmin, (req, res) => eventGropController.remove(req, res))
 
 
 //Exporting the router
